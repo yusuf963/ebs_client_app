2b591291ef9d094b7d05b8ae2a45434b3afe17f4
yusuf963 <mohammad963yusuf@gmail.com>
2022-07-10 20:43:02 +0100

    updste react version

--- src/components/Dummy.jsx
+++ src/components/Dummy.jsx
@@ -2,6 +2,7 @@ const Dummy =()=>{
   return(
     <a href="https://www.youtube.com/watch?v=DqtlR0y0suo" target={'_blank'}>
       <p>hello from other component</p>
+      <p>React version updated</p>
     </a>
   )
 }
