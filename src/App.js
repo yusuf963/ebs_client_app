f795c3fe3e98d71099c68d47525b398a99e63247
yusuf963 <mohammad963yusuf@gmail.com>
2022-07-23 19:47:05 +0100

    make change before building

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
+import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import Navigation from './components/navigation/Navigation'
+import SignIn from './pages/SignIn'
 
 function App() {
   return (
-    <>
-      <Navigation />
-      <Home />
-    </>
+    <Routes>
+      <Route path='/' element={<Navigation />}>
+        <Route index element={<Home />} />
+        <Route path='/sign-in' element={<SignIn />} />
+      </Route>
+    </Routes>
   );
 }
 
--- src/components/navigation/Navigation.jsx
+++ src/components/navigation/Navigation.jsx
@@ -1,18 +1,17 @@
-import {Outlet} from 'react-router-dom'
+import {Outlet,Link} from 'react-router-dom'
 import {ReactComponent as Logo} from '../../assests/crown_logo.svg'
-// import { Link } from 'react-router-dom'
 import './Navigation.scss'
 
 const Navigation = ()=>{
     return(
         <>
           <div className='navigation'>
-            <a className='logo-container' to='/' rel="noreferrer">
+            <Link className='logo-container' to='/' rel="noreferrer">
               <Logo className='nav-link'/>
-            </a>
+            </Link>
             <div className='nav-links-container'>
-              <a className='nav-link' to='/shop' rel="noreferrer">Shop</a>
-              <a className='nav-link' to='/sign-in'>Sign-in</a>
+              <Link className='nav-link' to='/shop' rel="noreferrer">Shop</Link>
+              <Link className='nav-link' to='/sign-in'>Sign-in</Link>
             </div>
           </div>
             <Outlet/>
